62ea18f38f7084ae907fed5fa99f830f30904a06
Студент <@sta200b1-203-18.ad.sfedu.ru>
2019-11-28 16:55:12 +0300

    DaysInMonth.pas

--- DateTimeMethods.pas
+++ DateTimeMethods.pas
@@ -5,7 +5,18 @@ begin
   Result:=(year mod 4 = 0);
 end;
 
-function DaysInMonth(month, year: integer): integer := 0;
+function DaysInMonth(month, year: integer): integer;
+begin
+  case month of
+    1,3,5,7,8,10,12:Result:=31;
+    4,6,9,11:Result:=30;
+  end;
+  if month=2 then
+    if IsLeapYear(year) then
+      Result:=29
+    else
+      Result:=28;
+end;
 function LaterInDay(p1, p2: DateTime): DateTime := p1;
 function LaterInYear(p1, p2: DateTime): DateTime := p1;
 function DaysInYear(year: integer): integer := 0;
